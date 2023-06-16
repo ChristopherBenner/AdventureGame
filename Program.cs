bab9457d22619dde9e30d0eb431700f6cf801ef6
= <=>
2023-06-16 19:33:46 -0400

    Very beginning of a text based adventure game

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Adventure
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Adventure Game!\nWelcome weary traveler");
+            Console.WriteLine("You are about to enter the headquarters of your arch nemesis");
+            Console.WriteLine("Can your find the key hidden in their lair");
+
+            string playerName;
+            Console.WriteLine("Before we begin, what is your name?\n");
+            playerName = Console.ReadLine();
+            Console.WriteLine($"Hello {playerName}, Good Luck beating your arch nemesis");
+
+        }
+    }
+}
